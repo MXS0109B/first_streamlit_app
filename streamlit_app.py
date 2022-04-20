6b391e3d741bc74ee720c66cd38df89c02ef8174
Manwinder Singh <103966561+manwinder1967@users.noreply.github.com>
2022-04-20 16:07:23 -0400

    Update streamlit_app.py
    Added more text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,9 @@ import streamlit
 streamlit.title('My Mom\'s New Healthy Dinner')
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
